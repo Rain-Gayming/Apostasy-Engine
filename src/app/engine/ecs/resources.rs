1447595967d22b07d601fee4e05dcd3baf16d049
rain-gayming <raingayming@gmail.com>
2025-11-10 22:52:25 +0000

    Moved resource stuff from systems.rs to resource.rs

--- src/app/engine/ecs/resources.rs
+++ src/app/engine/ecs/resources.rs
@@ -1,4 +1,70 @@
+use std::any::Any;
+use std::cell::RefMut;
+use std::ops::{Deref, DerefMut};
+use std::{
+    any::TypeId,
+    cell::{Ref, RefCell},
+    collections::HashMap,
+    marker::PhantomData,
+};
+
 use downcast_rs::{Downcast, impl_downcast};
+
+use crate::app::engine::ecs::systems::SystemParam;
 
 pub trait Resource: Downcast {}
 impl_downcast!(Resource);
+
+pub struct Res<'a, T: 'static> {
+    pub value: Ref<'a, Box<dyn Any>>,
+    _marker: PhantomData<&'a T>,
+}
+
+impl<T: 'static> Deref for Res<'_, T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        self.value.downcast_ref().unwrap()
+    }
+}
+
+impl<'res, T: 'static> SystemParam for Res<'res, T> {
+    type Item<'new> = Res<'new, T>;
+
+    fn retrieve<'r>(resources: &'r HashMap<TypeId, RefCell<Box<dyn Any>>>) -> Self::Item<'r> {
+        Res {
+            value: resources.get(&TypeId::of::<T>()).unwrap().borrow(),
+            _marker: PhantomData,
+        }
+    }
+}
+
+pub struct ResMut<'a, T: 'static> {
+    pub value: RefMut<'a, Box<dyn Any>>,
+    _marker: PhantomData<&'a mut T>,
+}
+
+impl<T: 'static> Deref for ResMut<'_, T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        self.value.downcast_ref().unwrap()
+    }
+}
+
+impl<T: 'static> DerefMut for ResMut<'_, T> {
+    fn deref_mut(&mut self) -> &mut T {
+        self.value.downcast_mut().unwrap()
+    }
+}
+
+impl<'res, T: 'static> SystemParam for ResMut<'res, T> {
+    type Item<'new> = ResMut<'new, T>;
+
+    fn retrieve<'r>(resources: &'r HashMap<TypeId, RefCell<Box<dyn Any>>>) -> Self::Item<'r> {
+        ResMut {
+            value: resources.get(&TypeId::of::<T>()).unwrap().borrow_mut(),
+            _marker: PhantomData,
+        }
+    }
+}
--- src/app/engine/ecs/systems.rs
+++ src/app/engine/ecs/systems.rs
@@ -45,65 +45,12 @@ impl Scheduler {
     }
 }
 
-trait SystemParam {
+pub trait SystemParam {
     type Item<'new>;
 
     fn retrieve<'r>(resources: &'r HashMap<TypeId, RefCell<Box<dyn Any>>>) -> Self::Item<'r>;
 }
 
-pub struct Res<'a, T: 'static> {
-    pub value: Ref<'a, Box<dyn Any>>,
-    _marker: PhantomData<&'a T>,
-}
-
-impl<T: 'static> Deref for Res<'_, T> {
-    type Target = T;
-
-    fn deref(&self) -> &T {
-        self.value.downcast_ref().unwrap()
-    }
-}
-
-impl<'res, T: 'static> SystemParam for Res<'res, T> {
-    type Item<'new> = Res<'new, T>;
-
-    fn retrieve<'r>(resources: &'r HashMap<TypeId, RefCell<Box<dyn Any>>>) -> Self::Item<'r> {
-        Res {
-            value: resources.get(&TypeId::of::<T>()).unwrap().borrow(),
-            _marker: PhantomData,
-        }
-    }
-}
-
-pub struct ResMut<'a, T: 'static> {
-    pub value: RefMut<'a, Box<dyn Any>>,
-    _marker: PhantomData<&'a mut T>,
-}
-
-impl<T: 'static> Deref for ResMut<'_, T> {
-    type Target = T;
-
-    fn deref(&self) -> &T {
-        self.value.downcast_ref().unwrap()
-    }
-}
-
-impl<T: 'static> DerefMut for ResMut<'_, T> {
-    fn deref_mut(&mut self) -> &mut T {
-        self.value.downcast_mut().unwrap()
-    }
-}
-
-impl<'res, T: 'static> SystemParam for ResMut<'res, T> {
-    type Item<'new> = ResMut<'new, T>;
-
-    fn retrieve<'r>(resources: &'r HashMap<TypeId, RefCell<Box<dyn Any>>>) -> Self::Item<'r> {
-        ResMut {
-            value: resources.get(&TypeId::of::<T>()).unwrap().borrow_mut(),
-            _marker: PhantomData,
-        }
-    }
-}
 macro_rules! impl_system {
     ($($params:ident),*) => {
         #[allow(unused_variables)]
