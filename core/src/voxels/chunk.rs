4dddd9c5af0901f00834e39ad619b2c9c5dd1875
rain <rainw2007@proton.me>
2026-04-20 11:04:14 +0100

    Added: vboxel definition

--- core/src/voxels/chunk.rs
+++ core/src/voxels/chunk.rs
@@ -1,4 +1,8 @@
 use apostasy_macros::Component;
 
+use crate::voxels::voxel::Voxel;
+
 #[derive(Clone, Component)]
-pub struct VoxelChunk {}
+pub struct VoxelChunk {
+    pub voxels: Vec<Voxel>,
+}
--- core/src/voxels/voxel.rs
+++ core/src/voxels/voxel.rs
@@ -1,2 +1,11 @@
+use hashbrown::HashMap;
+
 #[derive(Clone, Copy, Debug)]
 pub struct Voxel {}
+
+struct VoxelDefinition {
+    name: String,
+    namespace: String,
+    class: String,
+    // components: HashMap<String, Component>,
+}
