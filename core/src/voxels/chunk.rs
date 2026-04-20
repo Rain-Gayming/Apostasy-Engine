2f434989986fd1cdfc9f59816307daee2226ae67
rain-gayming <raingayming@proton.me>
2026-04-20 16:43:10 +0100

    Added: voxel definitions

--- core/src/voxels/chunk.rs
+++ core/src/voxels/chunk.rs
@@ -1,8 +1,24 @@
 use apostasy_macros::Component;
 
-use crate::voxels::voxel::Voxel;
+use crate::{
+    utils::flatten::flatten,
+    voxels::voxel::{VoxelDefinition, VoxelId, VoxelRegistry},
+};
 
 #[derive(Clone, Component)]
-pub struct VoxelChunk {
-    pub voxels: Vec<Voxel>,
+struct Chunk {
+    voxels: Box<[VoxelId; 32 * 32 * 32]>,
+}
+
+impl Chunk {
+    fn get_def<'a>(
+        &self,
+        x: u32,
+        y: u32,
+        z: u32,
+        registry: &'a VoxelRegistry,
+    ) -> &'a VoxelDefinition {
+        let id = self.voxels[flatten(x, y, z, 32 * 32 * 32)];
+        &registry.defs[id as usize]
+    }
 }
--- core/src/voxels/voxel.rs
+++ core/src/voxels/voxel.rs
@@ -1,11 +1,14 @@
 use std::any::TypeId;
 
 use anyhow::{Error, Result};
+use hashbrown::HashMap;
 
 use crate::objects::component::Component;
 
 #[derive(Clone, Copy, Debug)]
-pub struct Voxel {}
+pub struct Voxel {
+    pub id: VoxelId,
+}
 
 pub struct VoxelDefinition {
     pub name: String,
@@ -15,14 +18,14 @@ pub struct VoxelDefinition {
 }
 
 impl VoxelDefinition {
-    /// Checks if the node has a component of type T
+    /// Checks if the voxel has a component of type T
     pub fn has_component<T: Component + 'static>(&self) -> bool {
         self.components
             .iter()
             .any(|component| component.as_any().downcast_ref::<T>().is_some())
     }
 
-    /// Gets a component of type T from the node
+    /// Gets a component of voxel T from the node
     pub fn get_component<T: Component + 'static>(&self) -> Result<&T> {
         self.components
             .iter()
@@ -30,4 +33,11 @@ impl VoxelDefinition {
             .and_then(|c| c.as_any().downcast_ref())
             .ok_or(Error::msg("No Comopnent of type"))
     }
+}
+
+pub type VoxelId = u16;
+
+pub struct VoxelRegistry {
+    pub defs: Vec<VoxelDefinition>,
+    pub name_to_id: HashMap<String, VoxelId>,
 }
